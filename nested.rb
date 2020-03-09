e9caee9f26358e10a2293f91efca921daa5704c2
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-09 09:51:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,8 +129,8 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-	programmer_hash
+    }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	  programmer_hash
 
 end
